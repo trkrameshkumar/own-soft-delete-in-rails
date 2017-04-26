ae8127d32eff973230eef3e1f7a3429b35bfb418
Rameshkumar <trkrameshkumar@gmail.com>
2017-04-26 10:32:51 +0530

    initial commit

---
+++ db/migrate/20170426045517_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+    add_index :users, :deleted_at
+  end
+end
